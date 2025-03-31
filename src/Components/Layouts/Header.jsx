e9f6bc65baffe084f70629094a8c69c0eeb49c9c
Shubhi Kushwaha <Kushwahashubhi25@gmail.com>
2025-03-31 23:31:26 +0530

    second commit

---
+++ src/Components/Layouts/Header.jsx
@@ -0,0 +1,41 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import "../../Styles/HeaderStyle.css";
+import { Link } from 'react-router-dom';
+import Logo from "../../assets/logo.jpg";
+function Header(){
+  return (
+    <header>
+      <Navbar expand="lg" >
+      <Container>
+        <Navbar.Brand href="#home">
+          <Link>
+          <img src={Logo} alt="Logo" className='img-fluid'></img></Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/menu">Menu</Nav.Link>
+            <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
+            <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/">
+             <div className='cart'>
+              <i class="bi bi-bag fs-5"></i>
+              <em className='roundpoint'>2</em>
+             </div>
+            </Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      <br />
+      
+    </>
+  )
+}
+export default Header
---
+++ src/Components/Layouts/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Layout({Children}){
+  return (
+    <>
+    <Header/>
+    <div>{Children}</div>
+    <Footer/>
+    </>
+  )
+}
+export default Layout
