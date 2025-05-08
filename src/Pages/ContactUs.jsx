c971ea3cd812ad467c0a49fa138cda59a1f4d87f
Shubhi Kushwaha <Kushwahashubhi25@gmail.com>
2025-05-09 04:52:03 +0530

    adding contactus

---
+++ src/Pages/ContactUs.jsx
@@ -0,0 +1,13 @@
+export default function ContactUs() {
+  return (
+    <div className="container p-4">
+      <h2>Contact Us</h2>
+      <p>If you have any questions or concerns, please contact us:</p>
+      <ul>
+        <li>Email: kushwahashubhi25@gmail.com (or your actual email)</li>
+        <li>Phone: +91-9151257425</li>
+        <li>Address: Meal Mates Office, Your College Campus, India</li>
+      </ul>
+    </div>
+  );
+}
